de0415571f255b094c7e324b247dadfb832606cb
Kris <kristinaovchinnikova22@gmail.com>
2024-09-11 16:41:49 +0600

    app init creat a Number and creat function the cahngeNewNumber

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css'
+import Number from "./components/Number/Number.tsx";
+import {useState} from "react";
+
+
 
 const App = () => {
 
+    const [number , setNumber]  = useState <number[]>(
+        [ 5, 11, 16, 23, 32]
+    )
+
+    const changeNewNumber = () =>{
+        const NewNumberRandon: number = Math.round(Math.random() * 36 ) + 4;
+
+
+        setNumber([
+
+        ])
+    }
+
   return (
     <>
-      hello
+        <button onClick={changeNewNumber} type="button">New number</button>
+        <Number number = {number[0]}/>
+        <Number number = {number[1]}/>
+        <Number number = {number[2]}/>
+        <Number number = {number[3]}/>
+        <Number number = {number[4]}/>
     </>
   )
 };
---
+++ src/components/Number/Number.tsx
@@ -0,0 +1,12 @@
+import './Number.css';
+
+
+const Number = ({number}) => {
+    return (
+        <div className="person">
+            <h2>{number}</h2>
+        </div>
+    );
+};
+
+export default Number;
